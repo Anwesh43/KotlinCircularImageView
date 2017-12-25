6d4b73e453e8a07bbd5690aebe994f481cfb734c
Anwesh43 <anweshthecool0@gmail.com>
2017-12-25 23:59:02 +0530

    using ColorCircularImageView in activity

--- app/src/main/java/ui/anwesome/com/kotlincolorcircularimageview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincolorcircularimageview/MainActivity.kt
@@ -1,12 +1,16 @@
 package ui.anwesome.com.kotlincolorcircularimageview
 
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.colorcircularimageview.ColorCircularImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ColorCircularImageView.create(this,BitmapFactory.decodeResource(resources,R.drawable.trphy),Color.parseColor("#311B92"))
     }
 }
