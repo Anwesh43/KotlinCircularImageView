9834159140c57d58dab537dd6d4c586c17dd70de
Anwesh43 <anweshthecool0@gmail.com>
2017-12-25 23:51:35 +0530

    using renderer in view

--- colorcircularimageview/src/main/java/ui/anwesome/com/colorcircularimageview/ColorCircularImageView.kt
+++ colorcircularimageview/src/main/java/ui/anwesome/com/colorcircularimageview/ColorCircularImageView.kt
@@ -10,13 +10,15 @@ import android.view.MotionEvent
  */
 class ColorCircularImageView(ctx:Context,var bitmap: Bitmap,var color:Int):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = ColorCircularImageRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.draw(canvas,paint)
+        renderer.update()
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
