6ad49d541875cf84951e8b21f45bb04aca86a629
Anwesh43 <anweshthecool0@gmail.com>
2017-12-25 21:48:12 +0530

    adding ColorCircleImageView class

--- colorcircularimageview/src/main/java/ui/anwesome/com/colorcircularimageview/ColorCircularImageView.kt
+++ colorcircularimageview/src/main/java/ui/anwesome/com/colorcircularimageview/ColorCircularImageView.kt
@@ -1,5 +1,24 @@
 package ui.anwesome.com.colorcircularimageview
 
+import android.content.Context
+import android.view.View
+import android.graphics.*
+import android.view.MotionEvent
+
 /**
  * Created by anweshmishra on 25/12/17.
  */
+class ColorCircularImageView(ctx:Context,var bitmap: Bitmap):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
